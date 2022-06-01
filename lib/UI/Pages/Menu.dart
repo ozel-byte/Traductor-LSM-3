7b84603ed4c78be425106ba927304477e1d2484e
ozel-byte <ozel-byte@gmail.com>
2022-05-31 21:37:16 -0500

    TL-13: creacion del menu con los botones

--- lib/UI/Pages/Menu.dart
+++ lib/UI/Pages/Menu.dart
@@ -7,12 +7,47 @@ class Menu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Traductor-LSM"),
-      ),
-      body: Column(
+      body: Row(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: const [Text("Hola"), Text("Mundo")],
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SingleChildScrollView(
+                child: Text(
+                  "Selecione una opcion a probar",
+                  style: TextStyle(fontWeight: FontWeight.w300, fontSize: 25),
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              TextButton(
+                  style: ButtonStyle(
+                      padding: MaterialStateProperty.all(
+                          const EdgeInsets.symmetric(horizontal: 80)),
+                      backgroundColor: MaterialStateProperty.all(Colors.blue)),
+                  onPressed: () {
+                    Navigator.pushNamed(context, "lenguaje");
+                  },
+                  child: const Text(
+                    "Lenguaje senas",
+                    style: TextStyle(color: Colors.white),
+                  )),
+              TextButton(
+                  style: ButtonStyle(
+                      padding: MaterialStateProperty.all(
+                          const EdgeInsets.symmetric(horizontal: 100)),
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.green[300])),
+                  onPressed: () {
+                    Navigator.pushNamed(context, "guante");
+                  },
+                  child: const Text("Guante",
+                      style: TextStyle(color: Colors.white)))
+            ],
+          ),
+        ],
       ),
     );
   }
