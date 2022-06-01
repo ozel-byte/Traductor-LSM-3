3f5c58f25707ecdc2e6eaf855cb23c381be2578e
ozel-byte <ozel-byte@gmail.com>
2022-05-31 19:43:55 -0500

    TL-9: creacion del archivo SplashScreeen.dart

--- lib/UI/SplashScreen/SplashScreen.dart
+++ lib/UI/SplashScreen/SplashScreen.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -10,12 +11,38 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
-  Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-    return Scaffold();
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // Timer(Duration(milliseconds: 3000),
+    //     () => Navigator.pushNamed(context, 'pageselect'));
   }
-}//actions: [TextButton(onPressed: (){
-//Navigator.pushNamed(context, "Siguiente");
-//},
-//child: Icon(Icons.arrow_forward,
-//color: Colors.black)),],
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff7900ac),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Traductor LSM",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40,
+                    color: Colors.white),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              CircularProgressIndicator()
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
